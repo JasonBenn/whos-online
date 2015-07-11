c98c1acded09e1425034565422adb6dd9ecf2c4b
Jason Benn <jasoncbenn@gmail.com>
2015-07-11 01:38:12 -0700

    Styled, sorted results.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import express from 'express'
 import request from 'request'
+import moment from 'moment'
+// import { List } from 'immutable'
 
 let app = express()
 
@@ -8,14 +10,20 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static('public'));
 
+let ONLINE = /(\d+ minutes ago|just now\!)/
+let formatUnixTime = unixTime => moment.unix(unixTime).format("MMM Do")
+
 app.get('/', (req, res) => {
   // If you see a buffer-- what's the filetype? Remember the utf8 argument!
   fs.readFile('./whos-online.json', 'utf8', (err, data) => {
     let matches = JSON.parse(data).result.user_data.matches
     let onlineNow = []
-    for (let { id, subject } of matches) if (/(\d+ minutes ago|just now\!)/.test(subject.last_seen)) {
+    for (let { id, subject, match_date } of matches) if (ONLINE.test(subject.last_seen)) {
+      subject.matchDate = match_date
       onlineNow.push(subject)
     }
+    onlineNow = onlineNow.sort((a, b) => a.matchDate > b.matchDate ? -1 : 1)
+    onlineNow.forEach(match => match.matchDate = formatUnixTime(match.matchDate))
     res.render('index', {onlineNow})
   })
 })
@@ -27,3 +35,6 @@ app.listen(3000, () => console.log('up'))
 //     console.log(body)
 //   }
 // })
+
+// import moment from 'moment'
+// moment(1424753341, 'MMM Do, YY')
